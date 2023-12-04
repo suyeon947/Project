d592787afb253c92fa22dfe0400e7441b899ff9c
suyeon947 <suyeon947@gmail.com>
2023-12-04 21:36:48 +0900

    Modify4

--- RecipeMaker.cpp
+++ RecipeMaker.cpp
@@ -18,11 +18,7 @@ private:
     string Diets;
     string Exclude_Menu;
     string Taste;
-    string BuyList;
-    vector<std::string> MenuArray;
     vector<std::string> RecipeArray;
-
-    int days;
 
 public:
     void setIng(const string& UserIngs) {
@@ -41,24 +37,28 @@ public:
         Exclude_Menu = UserEcMenu;
     }
 
-    void setDays(const string& setdays){
-        days = stoi(setdays);
-    }
-
     void setTaste(const string& UserTaste) {
         Taste = UserTaste;
     }    
+
+    string getIng(){
+        return Ing;
+    }
+
+    string getMenu(){
+        return Menu;
+    }
 
     vector<std::string> getRecipe(){
         return RecipeArray;
     }
 
-    int getDays(){
-        return days;
+    string getAllergy(){
+        return Allergy;
     }
 
-    string getBuyList(){
-        return BuyList;
+    string getTaste(){
+        return Taste;
     }
 
     void print(const string& str){
@@ -80,11 +80,12 @@ public:
         requestData["model"] = "gpt-3.5-turbo";
         requestData["messages"][0]["role"] = "user";
         //cout << "제외된 메뉴들 : " << Exclude_Menu << endl;
-        requestData["messages"][0]["content"] = Exclude_Menu + "를 제외하고!!!!" + Ing + "(을)를 사용해서 만들 수 있는" + Taste + "음식은 뭐가 있어?" + num + 
-                                                "개만 추천해줘!!!모든 재료를 사용할 필요는 없고, 다른 재료를 사용해도 괜찮아. 그래도" + Allergy + 
+        requestData["messages"][0]["content"] = Exclude_Menu + "를 제외하고!!!!" + Ing + "(을)를 사용해서 만들 수 있는 " + Taste + " 음식은 뭐가 있어? " + num + 
+                                                "개만 추천해줘!!!모든 재료를 사용할 필요는 없고, 다른 재료를 사용해도 괜찮아. 그래도 " + Allergy + 
                                                 "는 사용하지마!! 설명 하지말고 음식만 나열해줘.";
         requestData["max_tokens"] = 3000;
         requestData["temperature"] = 0.5;
+        //cout << requestData["messages"][0]["content"] << endl;
 
         auto response = openai::chat().create(requestData);
 
@@ -93,12 +94,12 @@ public:
         //cout << Recipe << endl;
     }
 
-    void GenerageRecipe() {
+    void GenerageRecipe(const string& menu) {
         
         json requestData;
         requestData["model"] = "gpt-4-1106-preview";
         requestData["messages"][0]["role"] = "user";
-        requestData["messages"][0]["content"] =  "메뉴 : " + Menu + "\n레시피 알려줘. 1. 재료 2. 재료 손질법 3. 가열법 4. 조리법 , 이렇게 1. 2. 3. 4. 로 나누어 레시피 정렬해서 알려줘. 번역투 쓰지 말아줘.";
+        requestData["messages"][0]["content"] =  "메뉴 : " + menu + "\n레시피 알려줘. 1. 재료 2. 재료 손질법 3. 가열법 4. 조리법 , 이렇게 1. 2. 3. 4. 로 나누어 레시피 정렬해서 알려줘. 번역투 쓰지 말아줘.";
         requestData["max_tokens"] = 3000;
         requestData["temperature"] = 0.5;
 
@@ -125,112 +126,53 @@ public:
                        
         }
         RecipeArray.push_back("\n");
+        //print(RecipeArray);
+    }
+    
+
+};
+
+class DietHelper : public CookingHelper{
+private :
+    int days;
+    string BuyList;
+public :  
+    void setDays(const string& setdays){
+        days = stoi(setdays);
     }
 
-    void GenerageDiet() {
-
-        for(int i = 0; i < days - 1 ; i++){
-            string num = to_string(i);
-
-            
-            json requestData;
-            requestData["model"] = "gpt-3.5-turbo";
-            requestData["messages"][0]["role"] = "user";
-            requestData["messages"][0]["content"] =  "메뉴 : " + MenuArray[i] + "\n레시피 알려줘. 1. 재료 2. 재료 손질법 3. 가열법 4. 조리법 , 이렇게 1. 2. 3. 4. 로 나누어 레시피 정렬해서 알려줘. 번역투 쓰지 말아줘.";
-            requestData["max_tokens"] = 3000;
-            requestData["temperature"] = 0.5;
-
-            auto response = openai::chat().create(requestData);
-            string generatedText = response["choices"][0]["message"]["content"].get<std::string>();
-
-            std::stringstream ss(generatedText);
-            std::string line;
-            bool fisrt = true;
-            bool once = true;
-            while (std::getline(ss, line, '\n')) {
-                if(fisrt) {
-                    RecipeArray.push_back(line);
-                    fisrt = false;
-                }
-                if((line[0] >= '1' && line[0] <= '4')|| line[0] == '-' || line[0] == '\n'){
-                    RecipeArray.push_back(line);
-                    once = true;
-                }
-                if(line.empty()&&once){
-                    RecipeArray.push_back(line);
-                    once = false;
-                }
-            }
-            RecipeArray.push_back("\n");
-        }
+    int getDays(){
+        return days;
     }
 
-    void DietRecommend() {
+    string getBuyList(){
+        return BuyList;
+    }
+
+    void GenerateDietPlan() {
         string recommendedMenu;
-        int N = days-1;
-        if(N != 1) recommendedMenu += recomend_menu(N);
+        recommendedMenu = recomend_menu(days);
+        setMenu(recommendedMenu);
+        //cout << "메뉴들\n" << recommendedMenu << endl;
 
         std::stringstream ss(recommendedMenu);
         std::string line;
         while (std::getline(ss, line, '\n')) {
-            MenuArray.push_back(line);
+            GenerageRecipe(line);
         }
-        GenerageDiet();
     }
-
+    
     void GetList(){
-        string Menus = Menu;
-        for(int i=0; i<days-1; i++){
-            Menus += MenuArray[i];
-        }
+        string Menus = getMenu();
 
-        json requestData;
-        //std::cout << "Getlist에서 들어가는 Menu : " << Menus  << endl;
-        
-        requestData["prompt"] = Menus +  "를 만들기 위한 통합된 장바구니 리스트를 만들어줘. " + Ing + "는 이미 집에 있으므로 그 외에 필요한 재료를 나열해줘.";
+        json requestData;       
+        requestData["prompt"] = Menus +  "를 만들기 위한 통합된 장바구니 리스트를 만들어줘. " + getIng() + "는 이미 집에 있으므로 그 외에 필요한 재료를 나열해줘.";
         requestData["model"] = "text-davinci-003";
         requestData["max_tokens"] = 3000;
         requestData["temperature"] = 0.4;
 
         auto response = openai::completion().create(requestData);
-        //std::cout << "추천 메뉴입니다.\n" << response.dump(2) << '\n';
         BuyList = response["choices"][0]["text"].get<string>();
-    }
-
-    void MenuImage(const string& menu){
-        json requestData;
-        
-        
-        json requestString;
-        requestString["prompt"] = menu + "를 영어로 번역해줘."; // 프롬프트를 생성하고 설정해야 합니다.
-        requestString["model"] = "text-davinci-003";
-        requestString["max_tokens"] = 500;
-        requestString["temperature"] = 0.4;
-        auto response = openai::completion().create(requestString);
-        string menu_eng = response["choices"][0]["text"].get<string>();
-        menu_eng.erase(std::remove_if(menu_eng.begin(), menu_eng.end(), [](unsigned char ch) { return std::isspace(ch); }), menu_eng.end());
-        //cout << "공백이 있나요?" << menu_eng << endl;
-
-        requestData["prompt"] = menu_eng;
-        requestData["n"] = 1;
-        requestData["size"] = "256x256";
-
-        auto image = openai::image().create(requestData);
-
-        auto image_URL = image["data"][0]["url"];
-
-
-        string sys_cmd = "wget -O ./src/";
-        sys_cmd = sys_cmd + menu_eng + ".png ";
-        sys_cmd += nlohmann::to_string(image_URL);
-
-        system(sys_cmd.c_str());
-
-
-        cout << "Image Loding Done " << endl;
-
-        sys_cmd = "tiv -w 100 -h 100 ./src/" + menu_eng + ".png";
-        system(sys_cmd.c_str());
     }
 
     void saveRecipeToFile(int is_List){
@@ -255,140 +197,241 @@ public:
         file << std::endl << std::endl << "<<<<<레시피>>>>>" << std::endl; 
 
         if (file.is_open()) {
-            for (const auto& line : RecipeArray) {
+            for (const auto& line : getRecipe()) {
                 file << line << std::endl; // 각 섹션을 파일에 씁니다.
             }
             file.close();
             std::cout << "레시피가 식단표.txt 파일로 저장되었습니다." << std::endl;
-        } else {
+        } 
+        else {
             std::cout << "파일을 열 수 없습니다." << std::endl;
         }
     }
+
 };
 
-int main() {
-        CookingHelper helper;
-        string UserIngs, UserMenu, UserAllergy, UserMenuOK, Recommend, s_day, response;
+class HealthHelper : public CookingHelper{
+private :
+    string Nut;
+    string WOInfo;
+public : 
+    string get_Nut(){
+        return Nut;
+    }
 
-        std::cout << "알러지 등 섭취 시 주의해야 하는 음식을 알려주세요.\n\n" << "음식 : ";
+    string get_WOInfo(){
+        return WOInfo;
+    }
+
+    void FindNut(){
+        string menu = getMenu();
+        json requestData;       
+        requestData["prompt"] = menu +  "의 영양정보(칼로리, 탄수화물, 단백질, 지방)를 알려줘.";
+        requestData["model"] = "text-davinci-003";
+        requestData["max_tokens"] = 3000;
+        requestData["temperature"] = 0.4;
+    
+        auto response = openai::completion().create(requestData);
+        Nut = response["choices"][0]["text"].get<string>();
+    }
+
+    void FindWokrOut(){
+        string menu = getMenu();
+        json requestData;       
+        requestData["prompt"] = menu +  "를 소모할 수 있는 운동 및 운동별 시간을 알려줘";
+        requestData["model"] = "text-davinci-003";
+        requestData["max_tokens"] = 3000;
+        requestData["temperature"] = 0.4;
+    
+        auto response = openai::completion().create(requestData);
+        WOInfo = response["choices"][0]["text"].get<string>();
+    }
+
+
+
+};
+
+int main() {    
+        string Progarm, Retry;
+        CookingHelper Info;
+        string UserAllergy, UserTaste, UserIngs;
+        std::cout << "냉장고 속 재료 정보 및 사용자 정보를 수집합니다."<< std::endl << std::endl;
+
+        std::cout << "싫어하는 식재료를 알려주세요\n\n" << "음식 : ";
         getline(cin, UserAllergy);
-        helper.setAllergy(UserAllergy);
+        Info.setAllergy(UserAllergy);
 
         std::cout << "냉장고에 존재하는 재료를 5가지 이상 알려주세요.\n\n" << "재료 : ";
         getline(cin, UserIngs);
-        helper.setIng(UserIngs);
+        Info.setIng(UserIngs);
         std::cout<< std::endl;
 
-        Recommend = helper.recomend_menu();
-
-        std::cout << "추천 메뉴입니다.\n" << std::endl;
-        helper.print(Recommend);
-        std::stringstream ss(Recommend);
-        std::string line;
-        while (std::getline(ss, line, '\n')) {
-            
-            //helper.MenuImage(line);
-        }
-        std::cout << endl;
-        //std::cout  << Recommend << endl << endl;
-
-
-        cout << "다른 메뉴 선택을 원하시나요? (Y/N)\n";
-        getline(cin, UserMenuOK);
+        cout << "식사 취향을 알려주세요.\n" << "예시 : 육식 위주의/채식 위주의/고단백 위주의/저탄수화물 위주의/지중해식 위주의/한식 위주의\n" <<"취향 : ";
+        getline(cin, UserTaste);
+        Info.setTaste(UserTaste);
         std::cout<< std::endl;
 
-        int control = 1;
-        while(control == 1){
-            if(UserMenuOK == "Y"){
-                helper.setExclude(Recommend);
-                std::cout << "다른 추천 메뉴입니다." << std::endl; 
-                helper.print(helper.recomend_menu());
-                std::cout<< std::endl;
-                control = 0;
-            }
-            else if(UserMenuOK == "N"){
-                control = 0;
-            }
-            else{
-                std::cout << "(Y/N) 중에 골라 주세요" << std::endl;
-                getline(cin, UserMenuOK);
-            }
-        }
-
-        std::cout << "원하시는 메뉴를 입력해주세요.\n" << "메뉴 : ";
-        getline(cin, UserMenu);
-        helper.setMenu(UserMenu);
+        std::cout << "냉장고 속 재료 정보 및 사용자 정보를 이용해 만든 프로그램들이 있습니다.\n어떤 프로그램을 사용하길 원하시나요?\n"
+                    << "1.식단 플래너 2.운동 플래너 (숫자로 입력해 주세요)" << std::endl;
+        getline(cin, Progarm);
         std::cout << std::endl;
 
-        helper.GenerageRecipe(); 
-        helper.print(helper.getRecipe());
+        int P_ctl = 1;
+        while(P_ctl == 1){
+            if(Progarm == "1"){
+                // 식단 플래너 시작
+                P_ctl = 0;
+                DietHelper d_helper;
+                string s_day, response;
 
-        // 식단 추천 기능
-        std::cout << "식단 추천 기능을 이용하시겠습니까? (Y/N)" << std::endl;
-        getline(cin, response);
-        std::cout << std::endl;
+                d_helper.setAllergy(Info.getAllergy());
+                d_helper.setIng(Info.getIng());
+                d_helper.setTaste(Info.getTaste());
 
-        control = 1;
-        while(control == 1){
-            if(response == "Y"){
-                helper.setExclude(UserMenu);
-
-                std::cout << "며칠을 원하나요? (숫자 하나만 입력해주세요. 최대 7일 입니다.)" << std::endl;
+                std::cout << "식단은 며칠을 원하나요? (숫자 하나만 입력해주세요. 최대 7일 입니다.)" << std::endl;
                 getline(cin, s_day);
                 std::cout << std::endl;
-                helper.setDays(s_day);
-                control = 0;
-            }
-            else if(response == "N"){
-                control = 0;
-                break;
-            }
-            else{
-                cout << "(Y/N) 중에 골라 주세요" << endl;
-                getline(cin, response);
-            }
-        }
-        std::cout << "식단 추천" << std::endl;
-        helper.DietRecommend();
-        helper.print(helper.getRecipe());
-        std::cout << "식단 추천 완료" << std::endl;
+                d_helper.setDays(s_day);
 
-        std::cout << "장바구니 리스트를 보시겠습니까? (Y/N)" << std::endl;
-        getline(cin, response);
-        int controLL = 1;
-        while(controLL == 1){
-            if(response == "Y")
-            {
-                helper.GetList();
-                controLL = 0;
-            }
-            else if(response == "N"){
-                controLL = 2;
-                break;
-            }
-            else{
-                std::cout << "(Y/N) 중에 골라 주세요" << std::endl;
+                std::cout << "식단을 생성중입니다." << std::endl;
+                d_helper.GenerateDietPlan();
+                std::cout << "식단 생성 완료" << std::endl << std::endl;
+                d_helper.print(d_helper.getRecipe());
+                
+                std::cout << "장바구니 리스트를 보시겠습니까? (Y/N)" << std::endl;
                 getline(cin, response);
-            }
-        }
-        helper.print(helper.getBuyList());
+                int controLL = 1;
+                while(controLL == 1){
+                    if(response == "Y")
+                    {
+                        d_helper.GetList();
+                        controLL = 0;
+                    }
+                    else if(response == "N"){
+                        controLL = 2;
+                        break;
+                    }
+                    else{
+                        std::cout << "(Y/N) 중에 골라 주세요" << std::endl;
+                        getline(cin, response);
+                    }
+                }
+                d_helper.print(d_helper.getBuyList());
 
-        std::cout << "레시피와 장바구니 리스트를 파일로 저장해 드릴까요? (Y/N)" << std::endl << std::endl;
-        getline(cin, response);
-        int control3 = 1;
-        while(control3 == 1){
-            if(response == "Y")
-            {
-                helper.saveRecipeToFile(controLL);
-                control3 = 0;
+                std::cout << "레시피와 장바구니 리스트를 파일로 저장해 드릴까요? (Y/N)" << std::endl << std::endl;
+                getline(cin, response);
+                int control3 = 1;
+                while(control3 == 1){
+                    if(response == "Y")
+                    {
+                        d_helper.saveRecipeToFile(controLL);
+                        control3 = 0;
+                    }
+                    else if(response == "N"){
+                        return 0;
+                    }
+                    else{
+                        std::cout << "(Y/N) 중에 골라 주세요" << std::endl;
+                        getline(cin, response);
+                    }
+                }
+
+
+                std::cout << "\n다른 프로그램을 사용하길 원하나요? (Y/N)" << endl;
+                getline(cin, Retry);
+                int R_ctl = 1;
+                while(R_ctl == 1){
+                    if(Retry == "Y"){
+                        P_ctl = 2;
+                        R_ctl = 0;
+                    }
+                    else if(Retry == "N"){
+                        std::cout << "프로그램을 종료합니다" << endl;
+                        return 0;
+                    }
+                    else{
+                        std::cout << "(Y/N) 중에 골라 주세요" << std::endl;
+                        getline(cin, Retry);
+                    }
+                }
+
             }
-            else if(response == "N"){
-                return 0;
+            else if(Progarm == "2"){
+                // 운동 플래너 시작
+                P_ctl = 0;
+                HealthHelper h_helper;
+                string ntr, Recommend, menu, response;
+
+                h_helper.setAllergy(Info.getAllergy());
+                h_helper.setIng(Info.getIng());
+                h_helper.setTaste(Info.getTaste());
+
+                std::cout << "냉장고 속 재료 정보 및 사용자 정보를 통해 도출된 추천 메뉴 입니다." << std::endl;
+                Recommend = h_helper.recomend_menu();
+                h_helper.print(Recommend);
+                std::cout << std::endl;
+
+                std::cout << "영양 성분을 알고 싶은 메뉴를 입력해 주세요" << std::endl;
+                getline(cin, menu);
+                h_helper.setMenu(menu);
+                std::cout << std::endl;
+
+                std::cout << menu << "의 영양성분은 다음과 같습니다." << std::endl;
+                h_helper.FindNut();
+                h_helper.print(h_helper.get_Nut());
+
+                std::cout << menu << "를 소모할 수 있는 운동방법을 알려드릴까요? (Y/N)" << std::endl;
+                getline(cin, response);
+
+                int control = 1;
+                while(control == 1){
+                    if(response == "Y"){
+                        h_helper.FindWokrOut();
+                        std::cout << "운동법은 다음과 같습니다." << std::endl;
+                        h_helper.print(h_helper.get_WOInfo());
+                        control = 0;
+                    }
+                    else if(response == "N"){
+                        control = 0;
+                    }
+                    else{
+                        std::cout << "(Y/N) 중에 골라 주세요" << std::endl;
+                        getline(cin, response);
+                    }
+                }
+
+                std::cout << "\n다른 프로그램을 사용하길 원하나요? (Y/N)" << endl;
+                getline(cin, Retry);
+                int R_ctl = 1;
+                while(R_ctl == 1){
+                    if(Retry == "Y"){
+                        P_ctl = 2;
+                        R_ctl = 0;
+                    }
+                    else if(Retry == "N"){
+                        std::cout << "프로그램을 종료합니다" << endl;
+                        return 0;
+                    }
+                    else{
+                        std::cout << "(Y/N) 중에 골라 주세요" << std::endl;
+                        getline(cin, Retry);
+                    }
+                }
             }
             else{
-                std::cout << "(Y/N) 중에 골라 주세요" << std::endl;
-                getline(cin, response);
+                std::cout << "숫자만 입력해 주세요" << std::endl;
+                getline(cin, Progarm);
+            }
+
+            if(P_ctl == 2){
+                P_ctl = 1;
+                std::cout << "냉장고 속 재료 정보를 이용해 만든 프로그램들이 있습니다.\n어떤 프로그램을 사용하길 원하시나요?\n"
+                            << "1.식단 플래너 2.운동 플래너 (숫자로 입력해 주세요)" << endl;
+                getline(cin, Progarm);
+                std::cout << std::endl;
             }
         }
+
+        
     return 0;
 }
